036d56ebaa27a0598cd89bbe8e6fc43326be7ea9
Koda <koda@horizons.gg>
2025-06-18 13:40:36 +1000

    Update index.ts

--- app/modules/StatusBots/methods/index.ts
+++ app/modules/StatusBots/methods/index.ts
@@ -1,11 +1,13 @@
 import system from './system.ts'
 import dayz from './dayz.ts'
 import mc from './mc.ts'
+import valve from './valve.ts'
 
 
 
 export default {
     system,
     dayz,
-    mc
+    mc,
+    valve
 }
